3ee3e42a1d99d021d704d6824517ac9f0d2ee652
Akyidrian <akyidrian@gmail.com>
2018-04-19 11:47:06 -0700

    Street lamp

---
+++ 05-street-lamp.js
@@ -0,0 +1,28 @@
+/******************************************************
+ *  Note, the logic here is purposely backwards to a
+ *  street light. The nodebot-workshop wrongly expects
+ *  it be done this way with the circuit given. The LED
+ *  should really turn on when there is little light
+ *  (e.g. when this.value < 600).
+ *
+ *                PhotoR     10K
+ *         +5 o---/\/\/--.--/\/\/--.--o GND
+ *                       |         |
+ *     Pin A0 o-----------         |
+ *                                 |
+ *                330     LED      |
+ *      Pin 9 o--/\/\/----->|-------
+ *
+ ******************************************************/
+var five = require('johnny-five');
+var board = new five.Board();
+
+board.on('ready', function () {
+    var led = new five.Led(9);
+    var sensor = new five.Sensor("A0");
+
+	sensor.on("data", function() {
+		if(this.value > 600) { led.on(); }
+		else { led.off(); }
+	});
+});
